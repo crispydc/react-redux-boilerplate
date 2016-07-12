cfbf457427c94ef3961ca0787cb5c9ecb6585648
Katy Williamson <kwilliamson34@gmail.com>
2016-07-12 14:43:34 -0400

    removing remaining artifacts of old router

--- app/actions/__tests__/router-actions-test.js
+++
@@ -1,9 +0,0 @@
-import {changePage, CHANGE_PAGE} from '../router-actions';
-
-jest.unmock('../router-actions');
-
-describe('router-actions tests', () => {
-  it('creates create_page action', () => {
-    expect(changePage()).toEqual({type:CHANGE_PAGE});
-  })
-});
